10f38f308eee9a12560c94d23558668509cbd6fd
manato.kuroda <a7108100@gmail.com>
2021-01-18 07:14:00 +0900

    test: add test

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,10 +1,2 @@
 export const sum = (n1: number, n2: number) => n1 + n2
 
-export const sleep = (time: number, cb: Function) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      cb()
-      resolve(null)
-    }, time);
-  });
-}
