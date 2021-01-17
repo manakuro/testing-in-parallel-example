8cdad2b3165a6dfe8e6e7cfc0666e7e0c3643936
manato.kuroda <a7108100@gmail.com>
2021-01-18 07:45:06 +0900

    ci: fix

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  moduleDirectories: ['node_modules'],
+  testPathIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/'],
+  reporters: ["default", "jest-junit"]
+}
