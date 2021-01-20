e76d09e00ff59ea493b7b34f34e5b5db86020a0d
manato.kuroda <a7108100@gmail.com>
2021-01-20 12:50:42 +0900

    chore: test

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
+const testMatch = () => {
+  if (process.env.INTEGRATION)
+    return [`**/?(*.)+(intg).[jt]s?(x)`]
+
+  return [`**/?(*.)+(spec|test).[jt]s?(x)`]
+}
+
 module.exports = {
+  testMatch: testMatch(),
   testPathIgnorePatterns: ['/node_modules/'],
   transformIgnorePatterns: ['/node_modules/'],
 }
--- src/some3.test.js
+++ src/some3.test.js
@@ -1,8 +1,5 @@
-jest.setTimeout(30000);
-
 [...new Array(20)].forEach((_, i) => {
   test(`something ${i}`, async () => {
-    await new Promise((r) => setTimeout(r, 5000));
     expect(1 + 1).toBe(2);
   });
 })
